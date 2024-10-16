51c4959e668cd252b4bb8c5b0bef026c314761a8
Raphael Kreft <raphael.kreft@time-track.eu>
2024-10-16 21:13:38 +0200

    adapt deeplsd_gt_multiple_files.py for use on minidepth. adapt settings to euler

--- gluefactory/ground_truth_generation/deeplsd_gt_multiple_files.py
+++ gluefactory/ground_truth_generation/deeplsd_gt_multiple_files.py
@@ -21,25 +21,6 @@ from gluefactory.ground_truth_generation.generate_gt_deeplsd import (
 from gluefactory.models.lines.deeplsd import DeepLSD
 from gluefactory.settings import DATA_PATH, EVAL_PATH
 
-conf = {
-    "patch_shape": [800, 800],
-    "difficulty": 0.8,
-    "translation": 1.0,
-    "n_angles": 10,
-    "max_angle": 60,
-    "min_convexity": 0.05,
-}
-
-sp_conf = {
-    "max_num_keypoints": None,
-    "nms_radius": 4,
-    "detection_threshold": 0.005,
-    "remove_borders": 4,
-    "descriptor_dim": 256,
-    "channels": [64, 64, 128, 128, 256],
-    "dense_outputs": None,
-    "weights": None,  # local path of pretrained weights
-}
 
 homography_params = {
     "translation": True,
@@ -58,19 +39,19 @@ homography_params = {
 def get_dataset_and_loader(
     num_workers, distributed: bool = False
 ):  # folder where dataset images are placed
-    config = {
-        "name": "oxford_paris_mini",  # name of dataset class in gluefactory > datasets
+    dset_config = {
+        "name": "minidepth",  # name of dataset class in gluefactory > datasets
         "grayscale": True,  # commented out things -> dataset must also have these keys but has not
-        "preprocessing": {"resize": [800, 800]},
+        "preprocessing": None,
         "train_batch_size": 1,  # prefix must match split mode
         "num_workers": num_workers,
-        "split": "train",  # if implemented by dataset class gives different splits
+        "split": "all",  # if implemented by dataset class gives different splits
         "prefetch_factor": None if num_workers == 0 else 2,
     }
-    omega_conf = OmegaConf.create(config)
+    omega_conf = OmegaConf.create(dset_config)
     dataset = get_dataset(omega_conf.name)(omega_conf)
     loader = dataset.get_data_loader(
-        omega_conf.get("split", "train"), shuffle=False, distributed=distributed
+        omega_conf.get("split", "all"), shuffle=False, distributed=distributed
     )
     return loader
 
@@ -199,9 +180,6 @@ if __name__ == "__main__":
             pass
     out_folder_path = EVAL_PATH / args.output_folder
     out_folder_path.mkdir(exist_ok=True, parents=True)
-
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12355"
 
     print("OUTPUT PATH: ", out_folder_path)
     print("NUMBER OF HOMOGRAPHIES: ", args.num_H)
--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+import os
 
+my_scratch_space = Path(os.environ.get('SCRATCH'))
+
+cluster_team_folder = Path(
+    "/local/home/rkreft/shared_team_folder"
+)  # cluster team folder for 3dv
 root = Path(__file__).parent.parent  # top-level directory
-p = Path("/local/home/Point-Line/") # cluster team folder for 3dv
-DATA_PATH = p / "data"  # datasets and pretrained weights
-TRAINING_PATH = p / "outputs/training/"  # training checkpoints
-EVAL_PATH = p / "outputs/results/"  # evaluation results
+DATA_PATH = my_scratch_space / "data"  # datasets and pretrained weights
+TRAINING_PATH = cluster_team_folder / "outputs/training/"  # training checkpoints
+EVAL_PATH = my_scratch_space / "outputs/results/"  # evaluation results
