0db21c9c4db7428a99a8a6c48f3c9d1004a4fb2e
Raphael Kreft <raphael.kreft@time-track.eu>
2024-10-16 21:13:38 +0200

    adapt deeplsd_gt_multiple_files.py for use on minidepth. adapt settings to euler

--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -1,9 +1,12 @@
 from pathlib import Path
+import os
+
+my_scratch_space = Path(os.environ.get('SCRATCH'))
 
 cluster_team_folder = Path(
     "/local/home/rkreft/shared_team_folder"
 )  # cluster team folder for 3dv
 root = Path(__file__).parent.parent  # top-level directory
-DATA_PATH = cluster_team_folder / "data"  # datasets and pretrained weights
+DATA_PATH = my_scratch_space / "data"  # datasets and pretrained weights
 TRAINING_PATH = cluster_team_folder / "outputs/training/"  # training checkpoints
-EVAL_PATH = cluster_team_folder / "outputs/results/"  # evaluation results
+EVAL_PATH = my_scratch_space / "outputs/results/"  # evaluation results
